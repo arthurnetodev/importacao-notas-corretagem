8761fa269558a1dc65ab0c553d57caa8d2c79594
Arthur_Neto <arthur.neto@dell.com>
2025-03-10 23:27:22 -0300

    Logic improvement

--- fileprocessor.py
+++ fileprocessor.py
@@ -50,72 +50,44 @@ class FileProcessor:
         print(f"Process pdf as text:")
         #print(text)
         
-        # As informações sobre as ordens estarão entre as linhas "Negócios realizados" e "NOTA DE NEGOCIAÇÂO"
-        # A data estará na linha seguinte à linha "Data pregão"
         lines = text.split('\n')
 
+        # As informações sobre as ordens estarão entre as linhas "Negócios realizados" e "NOTA DE NEGOCIAÇÂO"
+        # A data estará na linha seguinte à linha "Data pregão"
+        
+        data_index = lines.index('Data pregão') + 1
+        self.dt = lines[data_index]
+
+        orders_section_init = lines.index('Negócios realizados')
+        end_of_header = lines.index('D/C', orders_section_init)
+        orders_section_end = lines.index('NOTA DE NEGOCIAÇÃO')
+        orders_lines = lines[end_of_header + 1:orders_section_end]
+
+        current_order = []
         orders = []
-        order_index = 0
-        order_entry = []
-        processing_orders = False
-        date_in_next_line = False
 
-        for i in range(len(lines)):
-            # Processar linha por linha
-            line = lines[i]
-
-            if "Data pregão" == line:
-                # Obter data na próxima linha
-                date_in_next_line = True
-                continue
-            
-            if "Negócios realizados" == line:
-                # Inicio da seção que contem as ordens
-                processing_orders = True
-                continue
-
-            if "NOTA DE NEGOCIAÇÃO" == line:
-                # Fim da seção que contem as ordens
-                processing_orders = False
-                continue
-
-            if date_in_next_line:
-                date_in_next_line = False
-                self.dt = line
-                continue
-
-            if processing_orders:
-                if order_index == 0:
-                    # Primeiro registro é o cabeçalho
-                    if line == "D/C":
-                        # Fim do cabeçalho, preparar para começar a processar a primeira ordem na sequencia
-                        order_index += 1
-                else:
-                    # Processando registro de ordem, armazenar as linhas no vetor até encontrar o fim do registro
-                    order_entry.append(line)
-                    # Somente procurar pelo campo que indica o fim do registro (contendo D ou C) a partir do quinto item, pois nos campos iniciais pode ter um campo com o valor C tambem
-                    if (len(order_entry) > 5):
-                        # Identificar fim do registro
-                        if (line == "D" or line == "C"):
-                            # Obter os dados da ordem para esse registro finalizado
-                            operation = order_entry[1]
-                            market_type = order_entry[2]
-                            title = order_entry[3]
-                            ticker = self.__convertTitle2Ticker(title)
-                            # O tamanho do vetor pode variar dependendo do tamanho do nome do ativo, então vamos pegar os ultimos 4 elementos com base no tamanho do vetor
-                            entry_last_index = len(order_entry)-1
-                            quantity = int(order_entry[entry_last_index-3])
-                            price = self.__convertStringToFloat(order_entry[entry_last_index-2])
-                            value = self.__convertStringToFloat(order_entry[entry_last_index-1])
-                            debitcredit = order_entry[entry_last_index]
-                            
-                            order = Order(ticker, operation, market_type, title, quantity, price, value, debitcredit)
-                            print(order)
-                            orders.append(order)
-
-                            # Inicia proximo registro
-                            order_entry = []
-                            order_index += 1
+        for line in orders_lines:
+            current_order.append(line)
+            # Somente procurar pelo campo que indica o fim do registro (contendo D ou C) a partir do quinto item, pois nos campos iniciais pode ter um campo com o valor C tambem
+            if (len(current_order) > 5):
+                if (line == "D" or line == "C"):
+                    if current_order:
+                        # Obter os dados da ordem para esse registro finalizado
+                        operation = current_order[1]
+                        market_type = current_order[2]
+                        title = current_order[3]
+                        ticker = self.__convertTitle2Ticker(title)
+                        # O tamanho do vetor pode variar dependendo do tamanho do nome do ativo, então vamos pegar os ultimos 4 elementos com base no tamanho do vetor
+                        entry_last_index = len(current_order)-1
+                        quantity = int(current_order[entry_last_index-3])
+                        price = self.__convertStringToFloat(current_order[entry_last_index-2])
+                        value = self.__convertStringToFloat(current_order[entry_last_index-1])
+                        debitcredit = current_order[entry_last_index]
+                        
+                        order = Order(ticker, operation, market_type, title, quantity, price, value, debitcredit)
+                        print(order)
+                        orders.append(order)
+                        current_order = []
 
         # Agrupar ordens do mesmo ticker e mesma operação (compra ou venda)
         grouped_orders = {}
@@ -130,7 +102,7 @@ class FileProcessor:
                 
         print("########################################################")
         return grouped_orders
-    
+
     def execute(self, pdf_path):
         orders = {}
         orders = self.__orders_table_as_text_processing(pdf_path)
